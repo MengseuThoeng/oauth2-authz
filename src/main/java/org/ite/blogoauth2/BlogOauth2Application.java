731acbce043d7a89d1db75690e754cdbd61441b4
MengseuThoeng <mengseu2004@gmail.com>
2024-09-03 10:29:21 +0700

    02-Sep-2024||Config Outh2 Login

---
+++ src/main/java/org/ite/blogoauth2/BlogOauth2Application.java
@@ -0,0 +1,13 @@
+package org.ite.blogoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogOauth2Application.class, args);
+    }
+
+}
