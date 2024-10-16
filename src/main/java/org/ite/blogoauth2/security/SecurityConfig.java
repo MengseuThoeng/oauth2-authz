272a63de948b53c32a04a6e617533bd1cc248876
MengseuThoeng <mengseu2004@gmail.com>
2024-10-16 09:12:04 +0700

    Final

--- src/main/java/org/ite/blogoauth2/security/SecurityConfig.java
+++ src/main/java/org/ite/blogoauth2/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,6 +41,12 @@ public class SecurityConfig {
                                 new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
                         )
                 );
+//        http.authorizeHttpRequests((authorize) -> authorize
+//                .requestMatchers("oauth2/token").permitAll()
+//                .anyRequest().authenticated()
+//        );
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
